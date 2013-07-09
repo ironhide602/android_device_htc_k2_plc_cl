5ba1fe7b65de88b96014db2cf1f98c9a9c04e77a
Simon Sickle <simonsimons34@gmail.com>
2013-07-09 14:31:34 +0000

    Add K2_PLC_CL changes.

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := k2_cl
+PRODUCT_RELEASE_NAME := k2_plc_cl
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/k2_cl/device_k2_cl.mk)
+$(call inherit-product, device/htc/k2_plc_cl/device_k2_plc_cl.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := k2_cl
-PRODUCT_NAME := cm_k2_cl
+PRODUCT_DEVICE := k2_plc_cl
+PRODUCT_NAME := cm_k2_plc_cl
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One SV
 PRODUCT_MANUFACTURER := HTC
